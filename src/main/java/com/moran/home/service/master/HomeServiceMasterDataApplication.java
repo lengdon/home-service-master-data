40ff5bec07b2b5090aee5c43d15cbca57a9543a0
Rahul <raahool_16@rediffmail.com>
2018-07-05 23:53:09 +0530

    Initial commit

---
+++ src/main/java/com/moran/home/service/master/HomeServiceMasterDataApplication.java
@@ -0,0 +1,12 @@
+package com.moran.home.service.master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeServiceMasterDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeServiceMasterDataApplication.class, args);
+	}
+}
---
+++ src/test/java/com/moran/home/service/master/HomeServiceMasterDataApplicationTests.java
@@ -0,0 +1,16 @@
+package com.moran.home.service.master;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HomeServiceMasterDataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
